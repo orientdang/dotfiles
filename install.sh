a1c533477c3a0f79b930baab968500335b613aed
dang-codelink <dang@codelink.io>
2021-05-18 11:48:40 +0700

    Backup at Tue May 18 11:48:40 +07 2021

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+mkdir -p ~/dotfiles && cd ~/dotfiles
+git clone git@github.com:orientdang/dotfiles.git ./
+# git pull origin master
+# Bachup nvim
+echo "COPYING ~/.config/nvim"
+# rm -rf .config && mkdir .config && mkdir .config/nvim 
+# cp -Rf ~/.config/nvim/ .config/nvim/
+cp -Rf .config/nvim/ ~/.config/nvim/
+
+# Every files are list in this directory will be automatically  
+# sync from source eg. ~/.bash_profile
+
+# these files/directory will not be sync
+exclude=(".git .config")
+
+for file in ~/dotfiles/.[^.]*; do
+	filename="$(basename "$file")"
+	if [[ ! " ${exclude[*]} " =~ "$filename" ]]; then
+		echo "COPYING ${filename}....."
+		cp -rf ${filename} ~/${filename} 
+	fi
+done
+
+echo "Install DONE"
+echo "============================="
